3008d7b61d2a715a563b4a3d4139c33016663990
drewjex <drewjex28+1@gmail.com>
2018-01-27 10:17:01 -0700

    cursor functionality

--- server2.js
+++ server2.js
@@ -22,7 +22,7 @@ var line_history = [];
 
 io.on('connection', function (socket) {
   
- // first send the history to the new client
+ // first send the history to the new client //change to do all at once with an array
  for (var i in line_history) {
     socket.emit('draw_line', { line: line_history[i] } );
  }
@@ -38,4 +38,9 @@ io.on('connection', function (socket) {
       // send line to all clients
       io.emit('draw_line', { line: data.line });
  });
+
+ socket.on('move_cursor', function (data) {
+    // send cursor to all clients
+    io.emit('move_cursor', { cursor: data.cursor });
+});
 });
